3cd843cb7950fbf5e77a49af7927360b079006a3
512143 <cheelle@vk.com>
2021-11-21 11:41:17 +0300

    Delite Boton bar

--- dear/lib/main.dart
+++ dear/lib/main.dart
@@ -1,9 +1,9 @@
-import 'package:dear/botonbar.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({Key key}) : super(key: key);
 
@@ -14,7 +14,9 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 class hame extends StatefulWidget {
+  
   hame({Key key}) : super(key: key);
 
   @override
@@ -22,12 +24,53 @@ class hame extends StatefulWidget {
 }
 
 class _hameState extends State<hame> {
+  List<Widget> smsList = [];
+  void createSingleSms(){
+  setState(() {
+     Widget text = Text("дорова ");
+    smsList.add(text);
+  });
+  }
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-      bottomNavigationBar: createBottonBar(size)
+      body: SafeArea(
+       child:ListView.builder(
+         itemBuilder: (context,index){
+           return smsList[index];
+         },
+         itemCount: smsList.length,
+       ) ,
+      ),
+      bottomNavigationBar: SizedBox(
+        width: size.width,
+        height:70.0 ,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            SizedBox(
+            width: size.width*0.8,
+            child: TextField(
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.bug_report_sharp),
+                labelText: "Введи техт сообщеения",
+                border: OutlineInputBorder(
+                  borderRadius:
+                      BorderRadius.all(Radius.circular(30)))))),
+          
+            IconButton(
+              icon: Icon(Icons.arrow_forward_ios),
+             onPressed:() {
+               
+             },
+              ),
+          ],
+        ) ,
+      ),
     );
-    
   }
 }
+
+
+
